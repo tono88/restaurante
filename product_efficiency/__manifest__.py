514ffdfa1457713a71eb42136a77002ccd68a6a6
tono88 <estuarsandoval@gmail.com>
2024-10-13 23:18:40 -0600

    Inicial

---
+++ product_efficiency/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Product Efficiency",
+    'version': '16.0.1',
+    'depends': [
+        'base',
+    ],
+    'author': "Author Name",
+    'category': '',
+    'description': """
+    Description text
+    """,
+    'licence': 'GPL-2',
+    # data files always loaded at installation
+    'data': [
+    ],
+    'images': ['static/src/images/main_screenshot.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
